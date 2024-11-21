7f6efd2947dc13cf02290e96dd4f29a6d600c93e
Asker Ordokov <89250000107@mail.ru>
2024-11-21 13:15:05 +0300

    endsWith

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,26 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 12;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        if (age < 0) {
+            return "Некорректный ввод";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        String str = String.valueOf(age);
+        if(str.equals("11") || str.equals("12") || str.equals("13") || str.equals("14")){
+            return "Вам " + age + " лет";
+        }
+
+        if (str.endsWith("1")) {
+            return "Вам " + age + " год";
+        } else if (str.endsWith("2") || str.endsWith("3") || str.endsWith("4")) {
+            return "Вам " + age + " года";
+        }
+        return "Вам " + age + " лет";
     }
 }
